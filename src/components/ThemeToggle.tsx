0263ea5fc7c29de20661f8757097502e6949e878
Artyom <sheludeshev.artyom@mail.ru>
2023-03-22 17:59:48 +0300

    Refactoring ThemeToggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,23 +6,25 @@ import { useTheme } from "next-themes";
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
 
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else if (theme === "dark") {
+      setTheme("light");
+    }
+  }
+
   return (
     <button
       className="group w-12 h-12 flex items-center justify-center
                 rounded-full border border-light-outline dark:border-dark-outline
-                bg-transparent hover:bg-light-surfaceVariant hover:dark:bg-dark-surfaceVariant"
-      onClick={() => {
-        if (theme === "light") {
-          setTheme("dark");
-        } else if (theme === "dark") {
-          setTheme("light");
-        }
-      }}
+                bg-transparent"
+      onClick={toggleTheme}
     >
       {theme === "dark" ? (
-        <SunLight className="group-hover:fill-white" />
+        <SunLight className="group-hover:fill-dark-onSurfaceVariant" />
       ) : (
-        <HalfMoon className="group-hover:fill-black" />
+        <HalfMoon className="group-hover:fill-light-onSurfaceVariant" />
       )}
     </button>
   );
