2fbac18f7ea72252bda8e7d93fa4c4d6b54c6238
Artyom <sheludeshev.artyom@mail.ru>
2023-03-22 18:18:31 +0300

    Implemented Headline text component

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -1,9 +1,26 @@
 import Display from '@/components/ui/text/Display'
+import Headline from '@/components/ui/text/Headline'
 
 const Documentation = () => {
   return (
     <>
-      <Display size={'large'}>Documentation page</Display>
+      <Display>Documentation page</Display>
+      
+      <div>
+        <Display size={'small'}>Text components</Display>
+
+        <div className='p-3 bg-light-surfaceVariant dark:bg-dark-surfaceVariant text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant rounded-xl flex flex-col gap-3'>
+          <Display size={'large'}>Display large</Display>
+          <Display size={'medium'}>Display medium</Display>
+          <Display size={'small'}>Display small</Display>
+        </div>
+
+        <div className='p-3 bg-light-surfaceVariant dark:bg-dark-surfaceVariant text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant rounded-xl flex flex-col gap-3'>
+          <Headline size={'large'}>Headline large</Headline>
+          <Headline size={'medium'}>Headline medium</Headline>
+          <Headline size={'small'}>Headline small</Headline>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/ui/text/Headline.tsx
+++ src/components/ui/text/Headline.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import { FC, HTMLAttributes } from "react";
 
-const Headline = () => {
-  return (
-    <div>Headline</div>
-  )
-}
+const headlineVariants = cva("text-light-onSurface dark:text-dark-onSurface", {
+  variants: {
+    size: {
+      large: "text-[32px] leading-10",
+      medium: "text-[28px] leading-9",
+      small: "text-2xl",
+    },
+  },
+  defaultVariants: {
+    size: "medium",
+  },
+});
 
-export default Headline
+interface Props
+  extends VariantProps<typeof headlineVariants>,
+    HTMLAttributes<HTMLHeadingElement> {}
+
+const Headline: FC<Props> = ({ children, size }) => {
+  return <h2 className={headlineVariants({ size })}>{children}</h2>;
+};
+
+export default Headline;
