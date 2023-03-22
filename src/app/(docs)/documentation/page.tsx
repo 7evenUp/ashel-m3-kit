6c1d9cf00df819db6424933f17c121c8b2934556
Artyom <sheludeshev.artyom@mail.ru>
2023-03-22 18:01:11 +0300

    Added documentation page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,11 @@
+import Display from '@/components/ui/text/Display'
+
+const Documentation = () => {
+  return (
+    <>
+      <Display size={'large'}>Documentation page</Display>
+    </>
+  )
+}
+
+export default Documentation
