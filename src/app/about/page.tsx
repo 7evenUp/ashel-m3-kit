9ca0e45dbdf8a1d754c6195f17bee18d2eb3882c
Artyom <sheludeshev.artyom@mail.ru>
2023-03-22 18:01:19 +0300

    Added About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Display from '@/components/ui/text/Display'
+import React from 'react'
+
+const About = () => {
+  return (
+    <Display>About page</Display>
+  )
+}
+
+export default About
