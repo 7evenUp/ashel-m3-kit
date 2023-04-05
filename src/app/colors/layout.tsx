43596cb5713adfceef59ef2e795ed6e89b9b288d
Artyom <sheludeshev.artyom@mail.ru>
2023-04-06 02:50:46 +0300

    formatted code

--- src/app/colors/layout.tsx
+++ src/app/colors/layout.tsx
@@ -9,17 +9,13 @@ export const metadata = {
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full">
-      
       <div className="w-[180px] fixed left-[100px] h-screen top-0 p-3 border-r bg-light-surface dark:bg-dark-surface border-light-outlineVariant dark:border-dark-outlineVariant text-light-onSurface dark:text-dark-onSurface flex flex-col gap-[1px]">
         <NavLink href={"/colors"} label="Overview" />
         <NavLink href={"/colors/color-roles"} label="Color roles" />
         <NavLink href={"/colors/surfaces"} label="Surfaces" />
       </div>
 
-      <div className="ml-[180px] p-3">
-      {children}
-      </div>
-      
+      <div className="ml-[180px] p-3">{children}</div>
     </div>
   );
 };
--- src/app/typography/layout.tsx
+++ src/app/typography/layout.tsx
@@ -9,7 +9,6 @@ export const metadata = {
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full">
-      
       <div className="w-[180px] fixed left-[100px] h-screen top-0 p-3 border-r bg-light-surface dark:bg-dark-surface border-light-outlineVariant dark:border-dark-outlineVariant text-light-onSurface dark:text-dark-onSurface flex flex-col gap-[1px]">
         <NavLink href={"/typography"} label="Overview" />
         <NavLink href={"/typography/display"} label="Display" />
@@ -19,10 +18,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <NavLink href={"/typography/label"} label="Label" />
       </div>
 
-      <div className="ml-[180px] p-3">
-      {children}
-      </div>
-      
+      <div className="ml-[180px] p-3">{children}</div>
     </div>
   );
 };
