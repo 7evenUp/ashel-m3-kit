2623e1de87835a4b60179ea77f383f982fcf4de3
Artyom <sheludeshev.artyom@mail.ru>
2023-04-06 14:16:22 +0300

    Swaped NavigationRail shadow to border

--- src/components/NavigationRail.tsx
+++ src/components/NavigationRail.tsx
@@ -6,7 +6,7 @@ const NavigationRail = () => {
   return (
     <div
       className="bg-light-surface dark:bg-dark-surface text-light-onSurface dark:text-dark-onSurface
-                fixed top-0 left-0 flex flex-col gap-3 h-screen min-w-[100px] items-center justify-between shadow-lg"
+                fixed top-0 left-0 flex flex-col gap-3 h-screen min-w-[100px] items-center justify-between border-r border-light-outlineVariant dark:border-dark-outlineVariant"
     >
       <nav className="flex flex-col items-center gap-3 justify-center mt-5">
         <NavLinkWithIcon href="/about" label="About" icon={<HomeSimple />} />
