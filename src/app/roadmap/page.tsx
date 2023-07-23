9399384a9dd7bac1f643866a1dab8d91f01501b0
Artyom <sheludeshev.artyom@mail.ru>
2023-07-23 19:33:25 +0300

    Updated roadmap

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -2,6 +2,8 @@ import Display from "@/shared/typography/Display"
 
 import Paragraph from "@/components/Paragraph"
 import InlineLink from "@/components/InlineLink"
+import { Checkbox } from "@/shared/ui/Checkbox"
+import Label from "@/shared/typography/Label"
 
 export const metadata = {
   title: "Ashel M3 Kit | Roadmap",
@@ -22,6 +24,37 @@ const About = () => {
         </InlineLink>{" "}
         🙂
       </Paragraph>
+
+      <div className="flex flex-col gap-3 mt-10">
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Templates (Парочка готовых UI шаблонов)</Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">CSS transitions according to M3</Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Tailwind config page</Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Add <b>npm</b> and <b>pnpm</b> variants for adding packages</Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Improve Guides section for <b>Forms</b></Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Continue implementing M3 components</Label>
+        </div>
+        <div className="flex items-center gap-4">
+          <Checkbox />
+          <Label size="large">Add English translations</Label>
+        </div>
+      </div>
     </main>
   )
 }
