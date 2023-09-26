cd1f9625ef1bc075e80987a302dfe1748b831430
Artyom <sheludeshev.artyom@mail.ru>
2023-09-26 21:32:01 +0300

    Added first M3 UI Template

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,4 +1,12 @@
+import Image from "next/image"
+
+import Heading from "@/components/Heading"
+import Paragraph from "@/components/Paragraph"
+
 import Display from "@/shared/typography/Display"
+
+import CosSinImage from "@/public/templates/cossin.png"
+import InlineLink from "@/components/InlineLink"
 
 export const metadata = {
   title: "Ashel M3 Kit | Templates",
@@ -9,6 +17,30 @@ const Templates = () => {
   return (
     <main className="p-10 flex flex-col gap-3">
       <Display>Templates</Display>
+      <Paragraph>
+        В данном разделе представлены UI шаблоны, которые раскрывают возможности
+        M3 Kit.
+      </Paragraph>
+
+      <div className="flex flex-col gap-1">
+        <Heading>CosSin</Heading>
+        <Paragraph>Пример UI для дашборда.</Paragraph>
+        <Image
+          className="max-w-[600px] my-2 rounded-xl border border-light-outlineVariant dark:border-dark-outlineVariant"
+          alt="CosSin template"
+          src={CosSinImage}
+          width={1920}
+          quality={100}
+        />
+        <div className="flex gap-4">
+          <InlineLink href="https://github.com/7evenUp/dashboard-m3">
+            Source code
+          </InlineLink>
+          <InlineLink href="https://dashboard-m3.vercel.app/">
+            Deploy
+          </InlineLink>
+        </div>
+      </div>
     </main>
   )
 }
